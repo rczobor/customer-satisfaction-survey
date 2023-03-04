0fbe79a36d9accae683346f0d7eaeb3a603934b9
Robert Czobor <czobor94@gmail.com>
2023-03-05 00:05:45 +0100

    fix: Remove example references

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,13 +1,11 @@
 import { router } from "../trpc";
 import { answerRouter } from "./answer";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 import { personRouter } from "./person";
 import { questionRouter } from "./question";
 import { recordRouter } from "./record";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   question: questionRouter,
   answer: answerRouter,
